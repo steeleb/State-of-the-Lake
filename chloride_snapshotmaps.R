e3348868810bb1685b09412c786bb33683914038
B Steele <steeleb@caryinstitute.org>
2022-10-14 11:59:20 -0600

    update to trib seasonality

--- chloride_snapshotmaps.R
+++ chloride_snapshotmaps.R
@@ -30,6 +30,160 @@ lmp <- st_as_sf(lmp_shortlist,
                 coords = c('lon_dd', 'lat_dd'),
                 crs = 'epsg:4326') 
 
+# 2020 march chloride in tribs deep dive ----
+lmp_cl_mar2020 <- lmp_cl %>% 
+  right_join(lmp_shortlist) %>% 
+  filter(year == 2020 & month == 3) %>% 
+  group_by(station, lon_dd, lat_dd, site_type, sub_site_type, Name) %>% 
+  filter(!is.na(value)) %>% 
+  summarise(value = mean(value)) %>% 
+  mutate(mean_name = as.integer(value)) %>% 
+  mutate(Name = paste0(Name, ' (', mean_name, ')'))
+
+lmp_cl_mar2020 <- st_as_sf(lmp_cl_mar2020, 
+                        coords = c('lon_dd', 'lat_dd'),
+                        crs = 'epsg:4326') 
+
+lmp_cl_mar2020 <- lmp_cl_mar2020 %>% 
+  filter(sub_site_type == 'tributary')
+
+lmp_cl_mar2020_stream_e = lmp_cl_mar2020 %>% 
+  filter(station == 830 | station == 835 | 
+           station == 805 | station == 800 | station == 788  | station == 760)
+lmp_cl_mar2020_stream_w = lmp_cl_mar2020 %>% 
+  filter(station != 830 & station != 835 & 
+           station != 805 & station != 800 & station != 788  & station != 760)
+
+st_bbox(watershed)
+
+ggplot() +
+  geom_sf(watershed, mapping = aes(), fill = 'white') +
+  geom_sf(streams, mapping = aes(), color = 'dark blue') +
+  geom_sf(waterbodies, mapping = aes(), fill = 'light blue') +
+  geom_sf(roads, mapping = aes(), color = 'light grey') +
+  geom_sf(lmp_cl_mar2020, mapping = aes(color = value), size = 3) +
+  labs(color = 'average\nchloride\n(mg/L)') +
+  scale_color_viridis_c() +
+  theme_void() +
+  annotation_north_arrow(width = unit(0.25, 'in')) +
+  labs(x = NULL, y = NULL, title = '2020 Late March\nTributary chloride') +
+  geom_sf_label_repel(lmp_cl_mar2020_stream_w, mapping = aes(label = Name), nudge_x = -1,nudge_y = -0.01, size = 1.75) +
+  geom_sf_label_repel(lmp_cl_mar2020_stream_e, mapping = aes(label = Name), nudge_x = 1,nudge_y = -0.001,  size = 1.75) +
+  # facet_grid(. ~ sub_site_type) +
+  theme(plot.title = element_text(hjust = 0.5, face = 'bold')) +
+  # theme(strip.text.x = element_text(size = 12, face = "bold")) +
+  theme(legend.position = 'bottom', legend.title = element_text(size = 10)) +
+  scale_x_continuous(limits = c(as.numeric(st_bbox(watershed)[1])-0.02, as.numeric(st_bbox(watershed)[3])+0.02))
+ggsave(file.path(dump_dir, 'chloridemar2020_tribs_map_labeled.jpg'),
+       height = 6,
+       width = 4,
+       dpi = 600,
+       units = 'in',
+       bg = 'white')
+
+# 2020 spring chloride in tribs deep dive ----
+lmp_cl_spr2020 <- lmp_cl %>% 
+  right_join(lmp_shortlist) %>% 
+  filter(year == 2020 & month >= 4 & month <=5) %>% 
+  group_by(station, lon_dd, lat_dd, site_type, sub_site_type, Name) %>% 
+  filter(!is.na(value)) %>% 
+  summarise(value = mean(value)) %>% 
+  mutate(mean_name = as.integer(value)) %>% 
+  mutate(Name = paste0(Name, ' (', mean_name, ')'))
+
+lmp_cl_spr2020 <- st_as_sf(lmp_cl_spr2020, 
+                           coords = c('lon_dd', 'lat_dd'),
+                           crs = 'epsg:4326') 
+
+lmp_cl_spr2020 <- lmp_cl_spr2020 %>% 
+  filter(sub_site_type == 'tributary')
+
+lmp_cl_spr2020_stream_e = lmp_cl_spr2020 %>% 
+  filter(station == 1415 | station == 1420 | station == 830 | station == 835 | 
+           station == 805 | station == 800 | station == 788  | station == 760)
+lmp_cl_spr2020_stream_w = lmp_cl_spr2020 %>% 
+  filter(station != 1415 & station != 1420 & station != 830 & station != 835 & 
+           station != 805 & station != 800 & station != 788  & station != 760)
+
+st_bbox(watershed)
+
+ggplot() +
+  geom_sf(watershed, mapping = aes(), fill = 'white') +
+  geom_sf(streams, mapping = aes(), color = 'dark blue') +
+  geom_sf(waterbodies, mapping = aes(), fill = 'light blue') +
+  geom_sf(roads, mapping = aes(), color = 'light grey') +
+  geom_sf(lmp_cl_spr2020, mapping = aes(color = value), size = 3) +
+  labs(color = 'average\nchloride\n(mg/L)') +
+  scale_color_viridis_c() +
+  theme_void() +
+  annotation_north_arrow(width = unit(0.25, 'in')) +
+  labs(x = NULL, y = NULL, title = '2020 Apr/May Tributary chloride') +
+  geom_sf_label_repel(lmp_cl_spr2020_stream_w, mapping = aes(label = Name), nudge_x = -1,nudge_y = -0.01, size = 1.75) +
+  geom_sf_label_repel(lmp_cl_spr2020_stream_e, mapping = aes(label = Name), nudge_x = 1,nudge_y = -0.001,  size = 1.75) +
+  # facet_grid(. ~ sub_site_type) +
+  theme(plot.title = element_text(hjust = 0.5, face = 'bold')) +
+  # theme(strip.text.x = element_text(size = 12, face = "bold")) +
+  theme(legend.position = 'bottom', legend.title = element_text(size = 10)) +
+  scale_x_continuous(limits = c(as.numeric(st_bbox(watershed)[1])-0.02, as.numeric(st_bbox(watershed)[3])+0.02))
+ggsave(file.path(dump_dir, 'chloridespring2020_tribs_map_labeled.jpg'),
+       height = 6,
+       width = 4,
+       dpi = 600,
+       units = 'in',
+       bg = 'white')
+
+# 2020 summer chloride in tribs deep dive ----
+lmp_cl_sum2020 <- lmp_cl %>% 
+  right_join(lmp_shortlist) %>% 
+  filter(year == 2020 & month >= 6 & month <=9) %>% 
+  group_by(station, lon_dd, lat_dd, site_type, sub_site_type, Name) %>% 
+  filter(!is.na(value)) %>% 
+  summarise(value = mean(value)) %>% 
+  mutate(mean_name = as.integer(value)) %>% 
+  mutate(Name = paste0(Name, ' (', mean_name, ')'))
+
+lmp_cl_sum2020 <- st_as_sf(lmp_cl_sum2020, 
+                           coords = c('lon_dd', 'lat_dd'),
+                           crs = 'epsg:4326') 
+
+lmp_cl_sum2020 <- lmp_cl_sum2020 %>% 
+  filter(sub_site_type == 'tributary')
+
+lmp_cl_sum2020_stream_e = lmp_cl_sum2020 %>% 
+  filter(station == 1415 | station == 1420 | station == 830 | station == 835 | 
+           station == 805 | station == 800 | station == 788  | station == 760)
+lmp_cl_sum2020_stream_w = lmp_cl_sum2020 %>% 
+  filter(station != 1415 & station != 1420 & station != 830 & station != 835 & 
+           station != 805 & station != 800 & station != 788  & station != 760)
+
+st_bbox(watershed)
+
+ggplot() +
+  geom_sf(watershed, mapping = aes(), fill = 'white') +
+  geom_sf(streams, mapping = aes(), color = 'dark blue') +
+  geom_sf(waterbodies, mapping = aes(), fill = 'light blue') +
+  geom_sf(roads, mapping = aes(), color = 'light grey') +
+  geom_sf(lmp_cl_sum2020, mapping = aes(color = value), size = 3) +
+  labs(color = 'average\nchloride\n(mg/L)') +
+  scale_color_viridis_c() +
+  theme_void() +
+  annotation_north_arrow(width = unit(0.25, 'in')) +
+  labs(x = NULL, y = NULL, title = '2020 Summer Tributary chloride') +
+  geom_sf_label_repel(lmp_cl_sum2020_stream_w, mapping = aes(label = Name), nudge_x = -1,nudge_y = -0.01, size = 1.75) +
+  geom_sf_label_repel(lmp_cl_sum2020_stream_e, mapping = aes(label = Name), nudge_x = 1,nudge_y = -0.001,  size = 1.75) +
+  # facet_grid(. ~ sub_site_type) +
+  theme(plot.title = element_text(hjust = 0.5, face = 'bold')) +
+  # theme(strip.text.x = element_text(size = 12, face = "bold")) +
+  theme(legend.position = 'bottom', legend.title = element_text(size = 10)) +
+  scale_x_continuous(limits = c(as.numeric(st_bbox(watershed)[1])-0.02, as.numeric(st_bbox(watershed)[3])+0.02))
+ggsave(file.path(dump_dir, 'chloridesummer2020_tribs_map_labeled.jpg'),
+       height = 6,
+       width = 4,
+       dpi = 600,
+       units = 'in',
+       bg = 'white')
+
+
 # 2020 chloride ----
 lmp_cl_2020 <- lmp_cl %>% 
   right_join(lmp_shortlist) %>% 
@@ -64,7 +218,7 @@ ggplot() +
   geom_sf(waterbodies, mapping = aes(), fill = 'light blue') +
   geom_sf(roads, mapping = aes(), color = 'light grey') +
   geom_sf(lmp_cl_2020_stream, mapping = aes(color = mean_cl_mgl), size = 3) +
-  labs(color = 'average\nsummer\nchloride\n(mg/L)') +
+  labs(color = 'average\nchloride\n(mg/L)') +
   scale_color_viridis_c() +
   theme_void() +
   labs(x = NULL, y = NULL, title = '2020 Tributary chloride') +
@@ -88,7 +242,7 @@ ggplot() +
   geom_sf(waterbodies, mapping = aes(), fill = 'light blue') +
   geom_sf(roads, mapping = aes(), color = 'light grey') +
   geom_sf(lmp_cl_2020_lake, mapping = aes(color = mean_cl_mgl), size = 3) +
-  labs(color = 'average\nsummer\nchloride\n(mg/L)') +
+  labs(color = 'chloride\n(mg/L)') +
   scale_color_viridis_c() +
   theme_void() +
   labs(x = NULL, y = NULL, title = '2020 in-lake chloride') +
--- chloride_summary.R
+++ chloride_summary.R
@@ -18,12 +18,33 @@ lmp_cl <- lmp %>%
   mutate(year = format(date, '%Y')) %>% 
   mutate(month = as.numeric(format(date, '%m')))
 
+lmp_ltjune_cl <- lmp_cl %>% 
+  filter(month < 6)
+
 #filter jun - sept
 lmp_summer_cl = lmp_cl %>% 
   filter(month >=6 & month <=9)
 
 lmp_summer_cl_select <- lmp_summer_cl %>% 
   left_join(lmp_locs, .) 
+
+lmp_ltjune_cl_select <- lmp_ltjune_cl %>% 
+  left_join(lmp_locs, .) 
+
+
+## aggregate and join by year/site for early season values ----
+lmp_cl_aggyearsite_ltjune <- lmp_ltjune_cl_select %>% 
+  filter(!is.na(value)) %>% 
+  group_by(year, station, site_type, sub_site_type, lat_dd, lon_dd) %>% 
+  summarize(n = n(),
+            med_cl_ugl = median(value),
+            max_cl_ugl = max(value),
+            mean_cl_ugl = mean(value),
+            thquan_cl_ugl = quantile(value, 0.75)) 
+
+lmp_cl_aggyearsite_ltjune <-lmp_cl_aggyearsite_ltjune %>% 
+  mutate(sub_site_type = factor(sub_site_type, levels = c( 'tributary', 'cove','deep')))
+
 
 ## aggregate and join by year/site ----
 lmp_cl_aggyearsite <- lmp_summer_cl_select %>% 
